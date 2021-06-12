25b8d907fc700fed8efca39a61ab98eeebbc3d16
unaguna <k.izumi.ysk@gmail.com>
2021-06-12 16:57:27 +0900

    ✅単レンジの端点の含有の有無の試験を作成

--- setrange/test/range_class.py
+++ setrange/test/range_class.py
@@ -30,3 +30,59 @@ class TestSetRangeClass:
         assert 'fa' in set_range
         assert 's' not in set_range
         assert 'sa' not in set_range
+
+    def test__srange_unit_int_ie__contain(self):
+        """単レンジ[int][)の含有演算をテストする。
+        """
+        set_range: SetRange[int] = srange(5, 10, '[)')
+
+        assert 4 not in set_range
+        assert 5 in set_range
+        assert 9 in set_range
+        assert 10 not in set_range
+        assert 4.0 not in set_range
+        assert 5.0 in set_range
+        assert 9.0 in set_range
+        assert 10.0 not in set_range
+
+    def test__srange_unit_int_ei__contain(self):
+        """単レンジ[int](]の含有演算をテストする。
+        """
+        set_range: SetRange[int] = srange(5, 10, '(]')
+
+        assert 5 not in set_range
+        assert 6 in set_range
+        assert 10 in set_range
+        assert 11 not in set_range
+        assert 5.0 not in set_range
+        assert 6.0 in set_range
+        assert 10.0 in set_range
+        assert 11.0 not in set_range
+
+    def test__srange_unit_int_ii__contain(self):
+        """単レンジ[int][]の含有演算をテストする。
+        """
+        set_range: SetRange[int] = srange(5, 10, '[]')
+
+        assert 4 not in set_range
+        assert 5 in set_range
+        assert 10 in set_range
+        assert 11 not in set_range
+        assert 4.0 not in set_range
+        assert 5.0 in set_range
+        assert 10.0 in set_range
+        assert 11.0 not in set_range
+
+    def test__srange_unit_int_ee__contain(self):
+        """単レンジ[int]()の含有演算をテストする。
+        """
+        set_range: SetRange[int] = srange(5, 10, '()')
+
+        assert 5 not in set_range
+        assert 6 in set_range
+        assert 9 in set_range
+        assert 10 not in set_range
+        assert 5.0 not in set_range
+        assert 6.0 in set_range
+        assert 9.0 in set_range
+        assert 10.0 not in set_range
