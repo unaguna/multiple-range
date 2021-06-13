ae891c022a9f8632949ae9289a6120f307f82dc2
unaguna <k.izumi.ysk@gmail.com>
2021-06-13 18:44:20 +0900

    ♻SetRangeUnit作成処理を統合
    2箇所に SetRangeUnit のサブクラスを選んで作成する処理が書かれていたため、統合

--- setrange/_range_function.py
+++ setrange/_range_function.py
@@ -1,6 +1,6 @@
 from typing import TypeVar
 
-from ._range_class import SetRange, SetRangeUnitII, SetRangeUnitEE, SetRangeUnitEI, SetRangeUnitIE
+from ._range_class import construct_unit, SetRange
 
 T = TypeVar('T')
 
@@ -11,25 +11,22 @@ def srange(start: T = None, end: T = None, edge: str = '[)', empty: bool = False
     if empty:
         return SetRange()
 
-    if edge == '[)':
-        if start < end:
-            return SetRange(SetRangeUnitIE(start, end))
-        else:
-            return SetRange()
-    elif edge == '[]':
-        if start <= end:
-            return SetRange(SetRangeUnitII(start, end))
-        else:
-            return SetRange()
-    elif edge == '(]':
-        if start < end:
-            return SetRange(SetRangeUnitEI(start, end))
-        else:
-            return SetRange()
-    elif edge == '()':
-        if start < end:
-            return SetRange(SetRangeUnitEE(start, end))
-        else:
-            return SetRange()
+    if edge[0] == '[':
+        include_start = True
+    elif edge[0] == '(':
+        include_start = False
+    else:
+        raise ValueError('edge must be \'[]\', \'[)\', \'(]\', or \'()\'.')
 
-    # TODO: edge が想定外である場合の処理
+    if edge[1] == ']':
+        include_end = True
+    elif edge[1] == ')':
+        include_end = False
+    else:
+        raise ValueError('edge must be \'[]\', \'[)\', \'(]\', or \'()\'.')
+
+    unit = construct_unit(start, end, include_start, include_end)
+    if unit is not None:
+        return SetRange(unit)
+    else:
+        return SetRange()
