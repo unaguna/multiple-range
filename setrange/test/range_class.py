ee7e29570edfc64ac93991581d0eea7c61681683
unaguna <k.izumi.ysk@gmail.com>
2021-06-26 22:54:09 +0900

    ✅既存のテストをparameterize
    無限についてテストを拡張しやすくするため。

--- setrange/test/range_class.py
+++ setrange/test/range_class.py
@@ -1,118 +1,96 @@
 import pytest
 
-from setrange import srange, SetRange
+from setrange import srange
 
 
 class TestSetRangeClass:
 
-    def test__srange_unit_int__str(self):
+    @pytest.mark.parametrize('set_range, expected_str', (
+            (srange(empty=True), '(empty)'),
+            (srange(5, 10, edge='[]'), '[5, 10]'),
+            (srange(5, 10, edge='[)'), '[5, 10)'),
+            (srange(5, 10, edge='(]'), '(5, 10]'),
+            (srange(5, 10, edge='()'), '(5, 10)'),
+    ))
+    def test__srange_unit_int__str(self, set_range, expected_str):
         """単レンジ[int]の文字列化をテストする。
         """
-        assert str(srange(empty=True)) == '(empty)'
-        assert str(srange(5, 10, edge='[]')) == '[5, 10]'
-        assert str(srange(5, 10, edge='[)')) == '[5, 10)'
-        assert str(srange(5, 10, edge='(]')) == '(5, 10]'
-        assert str(srange(5, 10, edge='()')) == '(5, 10)'
+        assert str(set_range) == expected_str
 
-    def test__srange_unit_int__equal(self):
+    @pytest.mark.parametrize('set_range_1, set_range_2, expected_equal', (
+            (srange(5, 10), srange(5, 10), True),
+            (srange(5, 10), srange(5, 9), False),
+            (srange(5, 10), srange(5, 10, edge='()'), False),
+            (srange(5, 10), srange(5, 10, edge='(]'), False),
+            (srange(5, 10), srange(5, 10, edge='[]'), False),
+    ))
+    def test__srange_unit_int__equal(self, set_range_1, set_range_2, expected_equal):
         """単レンジ[int]の等号演算をテストする。
         """
-        set_range_1: SetRange[int] = srange(5, 10)
-        set_range_2: SetRange[int] = srange(5, 10)
-        set_range_x: SetRange[int] = srange(5, 9)
-        set_range_ee: SetRange[int] = srange(5, 10, edge='()')
-        set_range_ei: SetRange[int] = srange(5, 10, edge='(]')
-        set_range_ii: SetRange[int] = srange(5, 10, edge='[]')
+        if expected_equal:
+            assert set_range_1 == set_range_2
+            assert set_range_2 == set_range_1
+        else:
+            assert set_range_1 != set_range_2
+            assert set_range_2 != set_range_1
 
-        assert set_range_1 == set_range_2
-        assert set_range_1 != set_range_x
-        assert set_range_1 != set_range_ee
-        assert set_range_1 != set_range_ei
-        assert set_range_1 != set_range_ii
+    @pytest.mark.parametrize('set_range, element, expected_contain', (
+            (srange(5, 10), 4, False),
+            (srange(5, 10), 5, True),
+            (srange(5, 10), 9, True),
+            (srange(5, 10), 10, False),
+            (srange(5, 10), 4.0, False),
+            (srange(5, 10), 5.0, True),
+            (srange(5, 10), 9.0, True),
+            (srange(5, 10), 10.0, False),
+            (srange(5, 10, '[]'), 4, False),
+            (srange(5, 10, '[]'), 5, True),
+            (srange(5, 10, '[]'), 10, True),
+            (srange(5, 10, '[]'), 11, False),
+            (srange(5, 10, '[]'), 4.0, False),
+            (srange(5, 10, '[]'), 5.0, True),
+            (srange(5, 10, '[]'), 10.0, True),
+            (srange(5, 10, '[]'), 11.0, False),
+            (srange(5, 10, '[)'), 4, False),
+            (srange(5, 10, '[)'), 5, True),
+            (srange(5, 10, '[)'), 9, True),
+            (srange(5, 10, '[)'), 10, False),
+            (srange(5, 10, '[)'), 4.0, False),
+            (srange(5, 10, '[)'), 5.0, True),
+            (srange(5, 10, '[)'), 9.0, True),
+            (srange(5, 10, '[)'), 10.0, False),
+            (srange(5, 10, '(]'), 5, False),
+            (srange(5, 10, '(]'), 6, True),
+            (srange(5, 10, '(]'), 10, True),
+            (srange(5, 10, '(]'), 11, False),
+            (srange(5, 10, '(]'), 5.0, False),
+            (srange(5, 10, '(]'), 6.0, True),
+            (srange(5, 10, '(]'), 10.0, True),
+            (srange(5, 10, '(]'), 11.0, False),
+            (srange(5, 10, '()'), 5, False),
+            (srange(5, 10, '()'), 6, True),
+            (srange(5, 10, '()'), 9, True),
+            (srange(5, 10, '()'), 10, False),
+            (srange(5, 10, '()'), 5.0, False),
+            (srange(5, 10, '()'), 6.0, True),
+            (srange(5, 10, '()'), 9.0, True),
+            (srange(5, 10, '()'), 10.0, False),
+            (srange('f', 's'), 'e', False),
+            (srange('f', 's'), 'ee', False),
+            (srange('f', 's'), 'f', True),
+            (srange('f', 's'), 'fa', True),
+            (srange('f', 's'), 's', False),
+            (srange('f', 's'), 'sa', False),
+    ))
+    def test__srange_unit__contain(self, set_range, element, expected_contain):
+        """単レンジの含有演算をテストする。
 
-    def test__srange_unit_int__contain(self):
-        """単レンジ[int]の含有演算をテストする。
-
-        int 型で指定した端点と比較できるオブジェクトであれば、含有するかどうか確かめられる。
+        端点と比較できるオブジェクトであれば、含有するかどうか確かめられる。
         """
-        set_range: SetRange[int] = srange(5, 10)
-
-        assert 4 not in set_range
-        assert 5 in set_range
-        assert 9 in set_range
-        assert 10 not in set_range
-        assert 4.0 not in set_range
-        assert 5.0 in set_range
-        assert 9.0 in set_range
-        assert 10.0 not in set_range
-
-    def test__srange_unit_str__contain(self):
-        """単レンジ[str]の含有演算をテストする。
-        """
-        set_range: SetRange[str] = srange('f', 's')
-
-        assert 'e' not in set_range
-        assert 'ea' not in set_range
-        assert 'f' in set_range
-        assert 'fa' in set_range
-        assert 's' not in set_range
-        assert 'sa' not in set_range
-
-    def test__srange_unit_int_ie__contain(self):
-        """単レンジ[int][)の含有演算をテストする。
-        """
-        set_range: SetRange[int] = srange(5, 10, '[)')
-
-        assert 4 not in set_range
-        assert 5 in set_range
-        assert 9 in set_range
-        assert 10 not in set_range
-        assert 4.0 not in set_range
-        assert 5.0 in set_range
-        assert 9.0 in set_range
-        assert 10.0 not in set_range
-
-    def test__srange_unit_int_ei__contain(self):
-        """単レンジ[int](]の含有演算をテストする。
-        """
-        set_range: SetRange[int] = srange(5, 10, '(]')
-
-        assert 5 not in set_range
-        assert 6 in set_range
-        assert 10 in set_range
-        assert 11 not in set_range
-        assert 5.0 not in set_range
-        assert 6.0 in set_range
-        assert 10.0 in set_range
-        assert 11.0 not in set_range
-
-    def test__srange_unit_int_ii__contain(self):
-        """単レンジ[int][]の含有演算をテストする。
-        """
-        set_range: SetRange[int] = srange(5, 10, '[]')
-
-        assert 4 not in set_range
-        assert 5 in set_range
-        assert 10 in set_range
-        assert 11 not in set_range
-        assert 4.0 not in set_range
-        assert 5.0 in set_range
-        assert 10.0 in set_range
-        assert 11.0 not in set_range
-
-    def test__srange_unit_int_ee__contain(self):
-        """単レンジ[int]()の含有演算をテストする。
-        """
-        set_range: SetRange[int] = srange(5, 10, '()')
-
-        assert 5 not in set_range
-        assert 6 in set_range
-        assert 9 in set_range
-        assert 10 not in set_range
-        assert 5.0 not in set_range
-        assert 6.0 in set_range
-        assert 9.0 in set_range
-        assert 10.0 not in set_range
+        if expected_contain:
+            assert element in set_range
+        else:
+            assert element not in set_range
 
     @pytest.mark.parametrize('target, is_empty', (
             (srange(5, 6, '[]'), False),
