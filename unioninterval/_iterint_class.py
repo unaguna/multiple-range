ed982291477ddc528faab6398a7482b2e3c28033
unaguna <k.izumi.ysk@gmail.com>
2021-07-12 20:37:53 +0900

    ♻逆方向のiterintの実装に備えてUnionRangeを抽象化

--- unioninterval/_iterint_class.py
+++ unioninterval/_iterint_class.py
@@ -1,50 +1,17 @@
+import abc
 import math
+from abc import abstractmethod
 
 from ._interval_class import UnionInterval
 from ._interval_function import interval
 
 
-def _normalize_int_interval(union_interval: UnionInterval) -> UnionInterval[int]:
-    """区間を正規化する。
-
-    整数値のみが入ることを前提に、区間を2整数 a, b を用いて [a, b) と表すよう変更する。
-
-    Parameters
-    ----------
-    union_interval
-        正規化する区間
-
-    Returns
-    -------
-    UnionInterval
-        正規化された区間
-    """
-    if not union_interval.is_interval:
-        return sum(map(_normalize_int_interval, union_interval), interval(empty=True))
-    else:
-        if union_interval.is_empty:
-            return union_interval
-
-        # start を決定
-        if union_interval.left_closed():
-            start = math.ceil(union_interval.inf())
-        else:
-            start = math.floor(union_interval.inf()) + 1
-
-        # end を決定
-        if union_interval.right_closed():
-            end = math.floor(union_interval.sup()) + 1
-        else:
-            end = math.ceil(union_interval.sup())
-
-        return interval(start, end, edge='[)')
-
-
-class UnionRange:
+class UnionRange(abc.ABC):
     _base_union_interval: UnionInterval[int]
 
     def __init__(self, union_interval: UnionInterval):
-        self._base_union_interval = _normalize_int_interval(union_interval)
+        # __eq__ のために正規化しておく
+        self._base_union_interval = self._normalize_int_interval(union_interval)
 
     def __eq__(self, other):
         if isinstance(other, UnionRange):
@@ -52,9 +19,52 @@ class UnionRange:
         else:
             return False
 
+    @abstractmethod
+    def __iter__(self):
+        ...
+
+    @abstractmethod
+    def _normalize_int_interval(self, union_interval: UnionInterval) -> UnionInterval[int]:
+        """区間を正規化する。
+
+        Parameters
+        ----------
+        union_interval
+            正規化する区間
+
+        Returns
+        -------
+        UnionInterval
+            正規化された区間
+        """
+
+
+class _UnionRangeForward(UnionRange):
+
     def __iter__(self):
         for interval_unit in self._base_union_interval:
             num = interval_unit.min()
             while num in interval_unit:
                 yield num
                 num = num + 1
+
+    def _normalize_int_interval(self, union_interval: UnionInterval) -> UnionInterval[int]:
+        if not union_interval.is_interval:
+            return sum(map(self._normalize_int_interval, union_interval), interval(empty=True))
+        else:
+            if union_interval.is_empty:
+                return union_interval
+
+            # start を決定
+            if union_interval.left_closed():
+                start = math.ceil(union_interval.inf())
+            else:
+                start = math.floor(union_interval.inf()) + 1
+
+            # end を決定
+            if union_interval.right_closed():
+                end = math.floor(union_interval.sup()) + 1
+            else:
+                end = math.ceil(union_interval.sup())
+
+            return interval(start, end, edge='[)')
--- unioninterval/_iterint_func.py
+++ unioninterval/_iterint_func.py
@@ -1,6 +1,6 @@
 from ._interval_class import UnionInterval
-from ._iterint_class import UnionRange
+from ._iterint_class import UnionRange, _UnionRangeForward
 
 
 def iterint(union_interval: UnionInterval) -> UnionRange:
-    return UnionRange(union_interval)
+    return _UnionRangeForward(union_interval)
