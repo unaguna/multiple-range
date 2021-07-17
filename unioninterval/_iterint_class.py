56d4170789f620b2ac53b11946538fc905742f0a
unaguna <k.izumi.ysk@gmail.com>
2021-07-17 16:25:48 +0900

    ✨逆順iterintを実装

--- unioninterval/_iterint_class.py
+++ unioninterval/_iterint_class.py
@@ -82,3 +82,40 @@ class _UnionRangeForward(UnionRange):
                 end = math.ceil(union_interval.sup())
 
             return interval(start, end, edge='[)')
+
+
+class _UnionRangeReversed(UnionRange):
+
+    def __repr__(self):
+        return f'iterint({repr(self._base_union_interval)}, reverse=True)'
+
+    def __iter__(self):
+        for interval_unit in reversed(self._base_union_interval):
+            num = interval_unit.max()
+            while num in interval_unit:
+                yield num
+                num = num - 1
+
+    def __reversed__(self):
+        raise NotImplementedError()
+
+    def _normalize_int_interval(self, union_interval: UnionInterval) -> UnionInterval[int]:
+        if not union_interval.is_interval:
+            return sum(map(self._normalize_int_interval, union_interval), interval(empty=True))
+        else:
+            if union_interval.is_empty:
+                return union_interval
+
+            # start を決定
+            if union_interval.left_closed():
+                start = math.ceil(union_interval.inf()) - 1
+            else:
+                start = math.floor(union_interval.inf())
+
+            # end を決定
+            if union_interval.right_closed():
+                end = math.floor(union_interval.sup())
+            else:
+                end = math.ceil(union_interval.sup()) - 1
+
+            return interval(start, end, edge='(]')
