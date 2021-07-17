6ba1b55cfeed8aa5abc688c959190d518c5f6249
unaguna <k.izumi.ysk@gmail.com>
2021-07-17 16:27:20 +0900

    ✅逆順 iterint のテストを修正

--- unioninterval/test/iterint.py
+++ unioninterval/test/iterint.py
@@ -33,11 +33,38 @@ class TestIterint:
         if expected_equal:
             assert iterint(interval1) == iterint(interval2)
             assert not iterint(interval1) != iterint(interval2)
-            assert iterint(interval1, reverse=True) == iterint(interval2, reverse=True)
-            assert not iterint(interval1, reverse=True) != iterint(interval2, reverse=True)
         else:
             assert not iterint(interval1) == iterint(interval2)
             assert iterint(interval1) != iterint(interval2)
+
+    @pytest.mark.parametrize('interval1, interval2, expected_equal', (
+        (interval(1, 10, edge='[]'), interval(1, 10, edge='[]'), True),
+        (interval(1, 10, edge='[)'), interval(1, 10, edge='[)'), True),
+        (interval(1, 10, edge='(]'), interval(1, 10, edge='(]'), True),
+        (interval(1, 10, edge='()'), interval(1, 10, edge='()'), True),
+        (interval(1, 10), interval(1, 9), False),
+        (interval(1, 10, edge='[]'), interval(1, 10, edge='[)'), False),
+        (interval(1, 10, edge='[]'), interval(1, 11, edge='[)'), True),
+        (interval(1, 10, edge='[]'), interval(1, 10, edge='(]'), False),
+        (interval(1, 10, edge='[]'), interval(0, 10, edge='(]'), True),
+        (interval(1, 10, edge='[]'), interval(0.9, 10.1, edge='[]'), True),
+        (interval(1, 10, edge='[]'), interval(0.9, 10.1, edge='[)'), True),
+        (interval(1, 10, edge='[]'), interval(0.9, 10.1, edge='(]'), True),
+        (interval(1, 10, edge='[]'), interval(0.9, 10.1, edge='()'), True),
+        (interval(1, 3, edge='[]') + interval(4, 6, edge='[]'), interval(1, 6, edge='[]'), True),
+        (interval(1, 3, edge='[)') + interval(4, 6, edge='[]'), interval(1, 6, edge='[]'), False),
+        (interval(1, 3, edge='[)') + interval(4, 6, edge='[]'),
+         interval(1, 3, edge='[)') + interval(4, 7, edge='[)'), True),
+        (interval(1, 2, edge='[)'), interval(singleton=1), True),
+        (interval(1, 2, edge='(]'), interval(singleton=2), True),
+    ))
+    def test__iterint__reverse__equal(self, interval1: UnionInterval, interval2: UnionInterval, expected_equal):
+        """逆順 iterint の等号演算をテストする。
+        """
+        if expected_equal:
+            assert iterint(interval1, reverse=True) == iterint(interval2, reverse=True)
+            assert not iterint(interval1, reverse=True) != iterint(interval2, reverse=True)
+        else:
             assert not iterint(interval1, reverse=True) == iterint(interval2, reverse=True)
             assert iterint(interval1, reverse=True) != iterint(interval2, reverse=True)
 
